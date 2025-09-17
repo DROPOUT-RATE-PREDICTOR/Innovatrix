8f30b445e8b210be7cebede7bb5ed2467b569f64
ojask3150 <ojaskasana6020@gmail.com>
2025-09-18 02:36:34 +0530

    Delete backend directory

--- backend/api/login.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import RequestCtrl from "./login.controller.js";
-
-const router = express.Router();
-router.route("/").post(RequestCtrl.apiCreateAccount);
-router.route("/signin").post(RequestCtrl.apiSignIn);
-
-export default router;
